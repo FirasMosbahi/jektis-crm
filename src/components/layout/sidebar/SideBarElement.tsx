e875abf10531762a6559d0736cb7bcf83cf6a0be
firas mosbahi <firas.mosbehi@insat.ucar.tn>
2024-02-21 01:10:29 +0100

    tables of reservation ui implemented

--- src/components/layout/sidebar/SideBarElement.tsx
+++ src/components/layout/sidebar/SideBarElement.tsx
@@ -14,8 +14,8 @@ export default function SideBarElement({
   return (
     <Link
       href={`/${element}`}
-      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-        pathname.includes(`/${element}`) && "bg-graydark dark:bg-meta-4"
+      className={`group relative flex text-white items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark ${
+        pathname.includes(`/${element}`) && "bg-graydark"
       }`}
     >
       {icon}
